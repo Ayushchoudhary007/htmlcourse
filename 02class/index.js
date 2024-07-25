e7a8db7e6315230af0fd2ee706968ecb8f699d7a
jatayushchoudhary@gmail.com <jatayushchoudhary@gmail.comssh-keygengit config --global user.email jatayushchoudhary@gmail.com>
2024-07-25 16:30:13 +0530

    js class02

---
+++ 02class/index.js
@@ -0,0 +1,53 @@
+// var name = "hello"
+// name = "ayush"
+// console.log(name)
+
+// let name = "hello"
+// name = "ayush"
+// console.log(name)
+
+// const name1 = "hello"
+// console.log(name1)
+
+// var name = "hello1"
+// var name = "world1"
+// console.log(name)
+
+// let name = "hello1"
+// let name = "world1"
+// console.log(name)
+
+// var name = "hello"
+// let name = "hell01"
+// console.log(name)
+
+// const obj = {"name":"Ayush jat","age":20}
+// console.log(obj)
+// console.log(obj["name"])
+// console.log(obj["age"])
+
+// const obj = {"first name":"Ayush","last name":"jat","age":20,"address":"jaipur"}
+// obj["address"] = "bhilwara"
+// console.log(obj)
+// console.log(obj["first name"])
+// console.log(obj["last name"])
+// console.log(obj["age"])
+// console.log(obj["address"])
+// const name = "hellp"
+// name = "there"
+// console.log(name)
+
+// const obj = {"first name":"Ayush","last name":"jat","age":20,"address":"jaipur"}
+// obj["address"] = null
+// obj["time"] = "10:00"
+// console.log(obj)
+// console.log(obj["first name"])
+// console.log(obj["last name"])
+// console.log(obj["age"])
+// console.log(obj["address"])
+
+// const arr = ["ayush",20,"shahpura",true]
+// console.log(arr)
+// console.log(arr[2])
+
+let num = 10
