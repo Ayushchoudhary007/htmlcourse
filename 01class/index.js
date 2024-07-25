e831baf36a3274bd988d01f1efa727c20acfb578
jatayushchoudhary@gmail.com <jatayushchoudhary@gmail.comssh-keygengit config --global user.email jatayushchoudhary@gmail.com>
2024-07-25 16:27:54 +0530

    js class01

---
+++ 01class/index.js
@@ -0,0 +1,47 @@
+// var name = "hello"
+// let name1 = "hello nothing will change"
+
+// console.log(name1)
+// console.log(name)  
+
+// const name2 = "so what "
+// console.log(name2)
+
+// var num = 1000
+// let num1 = 3000
+// const num4 = 5858
+// console.log(num)
+// console.log(num1)
+// console.log(num4)
+
+// var num3 = 1000.04
+// console.log(num3)
+
+// let num5 = 1000.4343
+// console.log(num5)
+// const num6 = 1000.03433
+// console.log(num6)
+
+// var name ="hello"
+// console.log(typeof name)
+// console.log(name)
+
+// let name1 ='hello'
+// console.log(typeof name1)
+// console.log(name1)
+
+// var name = 1000.003
+// console.log(typeof name)
+// console.log(name)
+
+
+// var name = 'true'
+// console.log(typeof name)
+// console.log(name)
+
+// var name = true
+// console.log(typeof name)
+// console.log(name)
+
+
+
