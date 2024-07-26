bfa03729c7df31abe2adc506abbd87b8a2a60d2d
jatayushchoudhary@gmail.com <jatayushchoudhary@gmail.comssh-keygengit config --global user.email jatayushchoudhary@gmail.com>
2024-07-26 14:04:02 +0530

    calculate

---
+++ 02class/assign.js
@@ -0,0 +1,23 @@
+// let num = 57
+// let char = "ch"
+// console.log(num + char)
+// console.log(num + 5)
+// console.log(num + 'so')
+// console.log(char * 5)
+// console.log(5 * char)
+// console.log('so' * 6)
+
+// let arr1 = [34,5,32,44,32]
+// let arr2 = [32,43,34,22,34]
+// console.log(arr1 + arr2)
+// console.log(arr1[3] + arr1[2] + arr2[0] + arr2[1])
+// console.log(arr1[4] - arr2[1])
+// console.log(arr1[1] * arr2[0])
+// console.log(arr1[3] % arr2[1])
+
+let cal = {'age':39,'month':7}
+let cal1 = {'age':19,'month':12}
+console.log(cal['age'] + cal1['age'])
+console.log(cal['age'] - cal1['age'])
+console.log(cal['age'] * cal1['age'])
+console.log(cal['age'] / cal1['age'])
