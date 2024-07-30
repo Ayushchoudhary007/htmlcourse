7560140b5afdeab307ad01b9495d7a323117271b
jatayushchoudhary@gmail.com <jatayushchoudhary@gmail.comssh-keygengit config --global user.email jatayushchoudhary@gmail.com>
2024-07-30 16:11:37 +0530

    30 july

---
+++ 06class/index.js
@@ -0,0 +1,85 @@
+// function hello(){
+//     console.log('my first function')
+// }
+// hello()
+// function ayush(a,b=0){
+//     console.log(a,b)
+// }
+// ayush("hello",23)
+// ayush("hello")
+// ayush(23)
+
+// function jat(...a){
+//     console.log(a)
+// }
+// jat("hello",20,true)
+
+// function ayush2(b,...a){
+//     console.log(b,a)
+// }
+// ayush2("hello",34,true)
+
+// function ayush3(...a){
+//     console.log(a[0][0])
+//     console.log(a[0][1])
+//     console.log(a[1]["name"])
+//     console.log(a[1]["age"])
+// }
+// let age = 23
+// let name = "hello"
+// let obj = {
+//     name,age
+// }
+// let arr = [name,age]
+// ayush3(arr,obj)
+
+// function ayush4(){
+//     console.log("heloo")
+//     return {name:'helo',age:23} 
+// }
+// let fun = ayush4()
+// console.log("fun",fun)
+
+
+// function ayush4(){
+//     console.log("heloo")
+//     let arr =["hello",23,true]
+//     let arr2 = ["hello1",33,false]
+//     return [arr,arr2]
+// }
+// let fun = ayush4()
+// console.log("fun",fun)
+
+// function ayush4(){
+//     console.log("heloo")
+//     let arr =["hello",23,true]
+//     let arr2 = ["hello1",33,false]
+//     return {arr,arr2}
+// }
+// let fun = ayush4()
+// console.log("fun",fun)
+
+// function ayush5(){
+//     return "hello"
+// }
+
+// let fun1 = ayush5()
+// console.log(fun1)
+
+function ayush5(){
+    try{
+            return "hello"
+    }
+    catch(error){
+        return {"error message ":error.message}
+    }
+}
+
+let fun1 = ayush5()
+if(fun1["error message"]){
+    console.log("code mistake")
+}
+else{
+    console.log(fun1)
+}
+
